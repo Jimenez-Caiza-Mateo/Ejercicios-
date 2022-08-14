f93eb99c08d04c8b6ab0b06dcc72ced00b7cb9f7
Jimenez-Caiza-Mateo <110944909+Jimenez-Caiza-Mateo@users.noreply.github.com>
2022-08-14 11:30:53 -0500

    Add files via upload

---
+++ Laboratorio.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+void toUpper(string &cadena)
+{
+  for(int i=0; cadena[i]!='\0' ; i++)
+  {
+    if (cadena.length() == 0)
+    {
+        return;
+    }
+    cadena[0] =toupper(cadena[0]);
+  }
+}
+int main()
+{
+string texto;
+toUpper(texto);
+
+cout<<"introduzca una cadena ";
+getline(cin,texto);
+
+for(int i=0; texto[i]!='\0' ; i++)
+{
+    if(texto[i-1]==' ')
+    {
+        texto[i] = toupper(texto[i]);
+    }
+    else
+    {
+        texto[i] = tolower(texto[i]);
+    }
+}
+    cout<<"la cadena modificada es "<<texto;
+}
